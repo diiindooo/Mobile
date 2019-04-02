bf1fb3e9efaec4717b39e4b8eacb71411b41938b
Dindo Tenoria <diiindooo7@gmail.com>
2019-04-02 15:46:14 +0800

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
+ // { path: 'dashboard', loadChildren: './members/dashboard/dashboard.module#DashboardPageModule' },
+  { path: 'members', canActivate: [AuthGuardService],
+      loadChildren: './members/member-routing.module#MemberRoutingModule'}, 
 ];
 
 @NgModule({
